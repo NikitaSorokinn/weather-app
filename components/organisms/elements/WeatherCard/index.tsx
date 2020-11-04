351a3aef620a2e3ee1248837ed9faacf7f41bf45
NikitaSorokinn <sorokinn1398@gmail.com>
2020-11-04 23:01:37 +0200

    Developvent 04.11.20

--- components/organisms/elements/WeatherCard/index.tsx
+++ components/organisms/elements/WeatherCard/index.tsx
@@ -4,6 +4,9 @@ import WeatherCardStyle from './style.module.scss'
 import CelsiusComponentStyle from './styleCelsiusComponent.module.scss'
 import WeatherNameComponentStyle from './styleWeatherNameComponent.module.scss'
 import WeatherAdditionalInfoStyle from './styleWeatherAdditionalInfo.module.scss'
+import WeatherCloudsStyle from './styleWeatherClouds.module.scss'
+import ShowplaceStyle from './styleShowplace.module.scss'
+import PlaceNameStyle from './stylePlaceName.module.scss'
 
 export const WeatherCard: React.FC = (): JSX.Element => {
 
@@ -19,7 +22,12 @@ export const WeatherCard: React.FC = (): JSX.Element => {
                         <WeatherAdditionalInfo name={'WIND'} value={'35%'}/>
                     </div>
                 </div>
-                <div></div>
+                <div className={WeatherCardStyle.WeatherCard__div__top__right}>
+                    <div className={WeatherCardStyle.WeatherCard__div__top__right__stars}/>
+                    <WeatherClouds/>
+                    <Showplace pictureSrc={'./big-ben-in-london.svg'}/>
+                    <PlaceName placeName={'LONDON'}/>
+                </div>
             </div>
             <div className={WeatherCardStyle.WeatherCard__div__bottom}>
 
@@ -59,6 +67,46 @@ export const WeatherAdditionalInfo: React.FC<IWeatherAdditionalInfo> = ({name, v
         <div className={WeatherAdditionalInfoStyle.WeatherAdditionalInfo__div}>
             <p className={WeatherAdditionalInfoStyle.WeatherAdditionalInfo__div__p}>{name}</p>
             <p className={WeatherAdditionalInfoStyle.WeatherAdditionalInfo__div__p}>{value}</p>
+        </div>
+    )
+}
+
+export const WeatherClouds: React.FC = (): JSX.Element => {
+    return (
+        <>
+            <img className={WeatherCloudsStyle.WeatherClouds__1} src={'./big-cloud.svg'} alt={' '}/>
+            <img className={WeatherCloudsStyle.WeatherClouds__2} src={'./big-cloud.svg'} alt={' '}/>
+            <img className={WeatherCloudsStyle.WeatherClouds__3} src={'./big-cloud.svg'} alt={' '}/>
+            <img className={WeatherCloudsStyle.WeatherClouds__4} src={'./big-cloud.svg'} alt={' '}/>
+            <img className={WeatherCloudsStyle.WeatherClouds__5} src={'./big-cloud.svg'} alt={' '}/>
+            <img className={WeatherCloudsStyle.WeatherClouds__6} src={'./big-cloud.svg'} alt={' '}/>
+        </>
+    )
+}
+
+export interface IShowplace {
+    pictureSrc: string
+}
+export const Showplace: React.FC<IShowplace> = ({pictureSrc}): JSX.Element => {
+    return (
+        <div className={ShowplaceStyle.Showplace__div}>
+            <img
+                className={ShowplaceStyle.Showplace__div__img}
+                src={pictureSrc}
+                alt=" "
+            />
+        </div>
+    )
+}
+
+export interface IPlaceName {
+    placeName: string
+}
+export const PlaceName: React.FC<IPlaceName> = ({placeName}): JSX.Element => {
+    return (
+        <div className={PlaceNameStyle.PlaceName__div}>
+            <p className={PlaceNameStyle.PlaceName__div__p}>{placeName}</p>
+            <div className={PlaceNameStyle.PlaceName__div__div}/>
         </div>
     )
 }
